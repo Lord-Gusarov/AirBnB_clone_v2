bcd3e720fac88aba8901be1ba924d6a21e4ff220
Gustavo Hornedo <2367@holbertonschool.com>
2021-04-18 22:57:18 +0000

    Add auto set-up for web-static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Sets up your web servers for the deployment of web_static
+# --To be executed/ran with 'sudo'
+apt-get update
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "<html><h1>Fake file, Nginx is Working!</h1></html>" | tee /data/web_static/releases/test/index.html
+ln -s /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+
+FILE="/etc/nginx/sites-available/default"
+toMatch=$(grep '^\s*server_name' $FILE)
+toAdd="$toMatch\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
+sed -i "s#^$toMatch#$toAdd#" $FILE
+service nginx restart
