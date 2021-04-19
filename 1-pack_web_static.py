f8d4552949eb5baba75e6152c9a98b9f16ec6431
Gustavo Hornedo <2367@holbertonschool.com>
2021-04-19 14:28:12 +0000

    Adds cript that generates an archive from the contents of dir 'web_static'

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pyhton3
+""" Creates .tgz archive from the contents of the web_static dir """
+from fabric.api import local, settings
+import datetime
+
+
+def do_pack():
+    """ Creates .tgz archive from the contents of the web_static dir """
+    dtime_stamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    pre_path = 'versions'
+    archive_path = '{}/web_static_{}.tgz'.format(pre_path, dtime_stamp)
+
+    with settings(warn_only=True):
+        local('mkdir -p {}'.format(pre_path))
+        result = local('tar -cvzf {} web_static'.format(archive_path))
+        if result.failed:
+            return None
+    return archive_path
