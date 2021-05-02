fd1c5a8349557d10987ae4ec47f73b73e8df1d1d
Gustavo Hornedo <2367@holbertonschool.com>
2021-05-02 18:29:52 +0000

    Adding Flask project

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_hbnb():
+    """Greets HBNB
+    """
+    return ('Hello HBNB!')
+
+
+if __name__ == '__main__':
+    app.run()
