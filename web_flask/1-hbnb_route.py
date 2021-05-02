28aa5f55d6d1d9a7ec4cf06d62dbaf5d8d143299
Gustavo Hornedo <2367@holbertonschool.com>
2021-05-02 18:33:22 +0000

    Adding task #1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """Greets
+    """
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """Greets HBNB
+    """
+    return ('HBNB')
+
+
+if __name__ == '__main__':
+    app.run()
