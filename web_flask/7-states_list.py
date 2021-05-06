fc8fe8ed528430c58c7acb24b70538137114fa5f
Gustavo Hornedo <2367@holbertonschool.com>
2021-05-06 03:05:40 +0000

    Adds to Flask the ability to List the States using a template

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.teardown_appcontext
+def teardwon(exception=None):
+    storage.close()
+
+@app.route('/states_list')
+def states_list():
+    """Returns list of States in html template"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+if __name__ == '__main__':
+    app.run()
